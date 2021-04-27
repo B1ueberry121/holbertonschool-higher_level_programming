ddbd23405ad0ecc1da5d790a6e3aec842c058ec8
B1ueberry121 <2710@holbertonschool.com>
2021-04-27 23:56:32 +0000

    part 12 substitution of numbers

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/bin/usr/python3
+def fizzbuzz():
+    x = 1
+    while x < 101:
+        if x % 3 != 0 and x % 5 != 0:
+            print("{:d}".format(x), end="")
+        else:
+            if x % 3 == 0:
+                print("Fizz", end="")
+            if x % 5 == 0:
+                print("Buzz", end="")
+        if x != 100:
+            print(" ", end="")
+        x += 1
+    print(" ", end="")
