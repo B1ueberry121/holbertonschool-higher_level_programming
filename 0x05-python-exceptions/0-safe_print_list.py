613a5f77dee0e1beee41cadabf2a69ff72c8f38a
B1ueberry121 <2710@holbertonschool.com>
2021-05-17 22:16:11 +0000

    typo

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,11 @@
-#!/usr/bin/pyhton3
-
-
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-
     numprint = 0
-
     for element in range(0, x):
         try:
             print(my_list[element], end="")
         except:
             break
         numprint = (numprint + 1)
-
     print()
     return numprint
