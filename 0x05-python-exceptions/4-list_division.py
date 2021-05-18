eef9d4ad37bb8d8e307ecfd41d9a3dcecbdd10a7
B1ueberry121 <2710@holbertonschool.com>
2021-05-18 01:28:04 +0000

    part 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = 0
+    newlist = []
+
+    for element in range(0, list_length):
+        try:
+            result = (my_list_1[element] / my_list_2[element])
+
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except TypeError:
+            print("wong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+
+        finally:
+            newlist.append(result)
+
+    return newlist
