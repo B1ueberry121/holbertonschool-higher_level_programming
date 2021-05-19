edf8144fffa6ebc67f2312b4149c053d503d86fe
B1ueberry121 <2710@holbertonschool.com>
2021-05-19 01:48:46 +0000

    changig order

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,8 +5,10 @@
 class Square:
     '''This class contains a size attribute'''
     def __init__(self, size=0):
-        if not type(size) is int:
+        '''This methods creates a new instance of square'''
+        if type(size) is int:
+            if size < 0:
+                raise ValueError("size must be => 0")
+            self.__size = size
+        else:
             raise TypeError("size must be an integer")
-        if (size < 0):
-            raise ValueError("size must be >=0")
-        self.__size = size
