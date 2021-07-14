0b04b81798b720f726c76b6b4fc9cd066c9a9fc1
B1ueberry121 <2710@holbertonschool.com>
2021-07-14 03:05:46 +0000

    typo

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Inserts a new row in "first_table" from "hbtn_0c_0"
-INSERT INTO 'first_table' ('id', 'name') VALUES (89, "Holberton School");
+INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
