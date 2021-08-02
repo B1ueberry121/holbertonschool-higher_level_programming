5a249e607a42c03f56d70b1080ad59ef93383665
B1ueberry121 <2710@holbertonschool.com>
2021-08-02 18:55:04 +0000

    semi

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const myArgv = process.argv.slice(2);
 if (myArgv.length === 0) {
-console.log('No argument');
+  console.log('No argument');
 } else if (myArgv.length === 1) {
-console.log('Argument found');
+  console.log('Argument found');
 } else {
-console.log('Arguments found');
+  console.log('Arguments found');
 }
