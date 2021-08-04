19ee07d68e5ddf2179f7df14c191096ab2cab66c
B1ueberry121 <2710@holbertonschool.com>
2021-08-04 13:13:01 +0000

    Using print func

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (!isNaN(h) && !isNaN(w) && (h > 0 && w > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let y = 0; y < this.height; ++y) {
+      for (let x = 0; x < this.width; ++x) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+};
