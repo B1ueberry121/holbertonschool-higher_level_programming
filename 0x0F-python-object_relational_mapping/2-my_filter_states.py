07a225d115f4f2b39ede70d046c7d7af1bc168c5
B1ueberry121 <2710@holbertonschool.com>
2021-08-09 02:13:36 +0000

    Filters states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' Lists all state contained in the the db "hbtn_0e_0_usa" '''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    dbname = sys.argv[3]
+    stateName = sys.argv[4]
+
+    db = MySQLdb.connect("localhost", user, pswd, dbname)
+
+    cursor = db.cursor()
+    sql = "SELECT * FROM states \
+            WHERE name LIKE BINARY '{}' ;".format(stateName)
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for item in results:
+            print(item)
+    except:
+        print("Failed to fetch data")
+    db.close()
