5e91f7aa9ed91a44d70e1324df88fd12d87e8289
B1ueberry121 <2710@holbertonschool.com>
2021-08-10 14:08:24 +0000

    First ORM

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Contains the State class '''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class State(Base):
+    ''' Represents the table states from db "hbtn_0e_4_usa" '''
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+    def __repr__(self):
+        ''' Method for string representation '''
+        return "State: {}, {}".format(self.name, self.id)
