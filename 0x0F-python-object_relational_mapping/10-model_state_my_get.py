c40854293e11987d85813a133e62f319c9531436
B1ueberry121 <2710@holbertonschool.com>
2021-08-10 15:59:40 +0000

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+''' Prints the State object with the name \
+        passed as argument from the db '''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects import postgresql
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    stateName = sys.argv[4]
+    try:
+        stateNameP = stateName.split(" ", 1)
+    except:
+        raise ValueError('Incorrect format')
+    query = session.query(State).filter(text("name='{}'"
+                                        .format(stateNameP[0])))
+    state = query.first()
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
