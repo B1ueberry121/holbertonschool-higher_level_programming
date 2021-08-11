f534e9067d7b68eb59df87f488cd6854e4157f7a
B1ueberry121 <2710@holbertonschool.com>
2021-08-11 13:37:39 +0000

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' Adds the State object "Louisiana" to the db '''
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects import postgresql
+from sqlalchemy.exc import SQLAlchemyError
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    data = "Louisiana"
+    try:
+        row = State(name="Louisiana")
+        session.add(row)
+        session.commit()
+        print(row.id)
+    except SQLAlchemyError as e:
+        print(e)
+    finally:
+        session.close()
+
