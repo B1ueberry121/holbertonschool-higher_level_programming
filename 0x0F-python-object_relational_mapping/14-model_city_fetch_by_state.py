d85817e0225f11440db54e86ef49dd10187f2aac
B1ueberry121 <2710@holbertonschool.com>
2021-08-12 02:28:13 +0000

    task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Lists all State objects from the database '''
+
+import sys
+from model_state import State, Base
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State, City).join(City)
+    for _s, _c in states.all():
+        print("{}: ({}) {}".format(_s.name, _c.id, _c.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' Contains the City class '''
+
+from model_state import State, Base
+from sqlalchemy import ForeignKey, Column, String, Integer
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """ Class represents City table pf database """
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
