4eae313224bd6ae0ac93a1187279510f012e5945
B1ueberry121 <christianlaboy839@gmail.com>
2021-08-26 18:17:18 +0000

    new fix

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Takes a URL as an argument
-# Sends a GET request to the URL
-# Displays the body of the response
-curl -s --header "X-HolbertonSchool-User-Id:98" $1
+# A header variable with the value 98
+curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
