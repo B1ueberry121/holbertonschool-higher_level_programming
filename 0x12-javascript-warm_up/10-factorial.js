6278e2005fbdd8cc81c74e4332ecb7b93764c752
B1ueberry121 <christianlaboy839@gmail.com>
2021-09-20 20:44:19 +0000

    task 10-17

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const intArg = parseInt(argv[2], 10);
+
+function factorial (num) {
+  if (isNaN(num)) {
+    return 1;
+  }
+
+  if (num === 0) {
+  return 1;
+  }
+
+  num *= factorial(num - 1);
+  return num;
+}
+
+console.log(factorial(intArg));
