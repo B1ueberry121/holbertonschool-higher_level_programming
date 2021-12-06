81575d5d4bfc378968cd166b2a134dff9c904abc
Christian Laboy <2710@holbertonschool.com>
2021-12-06 16:39:01 -0400

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-module.exports.callMeMoby = function (x, theFunction) {
-  for (let index = 0; index < x; index++) {
-    theFunction();
-  }
-};
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const ara = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let index = 0; index < ara.length; index++) {
-  console.log(ara[index]);
-}
